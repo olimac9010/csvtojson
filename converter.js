f65ff2fe9fe8f60408fb76de93f3889435ba8f8c
olimac9010 <49177590+olimac9010@users.noreply.github.com>
2019-10-25 09:05:16 -0400

    Initials files

---
+++ converter.js
@@ -0,0 +1,38 @@
+const fs = require('fs')
+const readLine = require('readline')
+
+const csvFileName = 'customer-data.csv'
+const jsonFileName = 'customer-data.json'
+let isHeader = true;
+let headers={}
+let buffer= '['
+
+let reader = readLine.createInterface({
+    input: fs.createReadStream(csvFileName)
+})
+
+reader.on('line',(row)=>{
+    let data = row.split(',')
+    let count = 0
+    let jsonObject= '{\n'
+
+    if(isHeader){
+        headers = data
+    } 
+    else {
+    headers.forEach((element,index) => {
+        jsonObject += " "+element + ': "' + data[index] + '",\n'
+    })
+    jsonObject = jsonObject.substr(0,jsonObject.length-2)
+    jsonObject += '\n},\n'
+    buffer+=jsonObject
+    }
+    isHeader=false
+    
+})
+
+reader.on('close',()=>{
+    buffer = buffer.substr(0,buffer.length-2);
+    buffer+=']'
+    fs.writeFileSync(jsonFileName,buffer)
+})
